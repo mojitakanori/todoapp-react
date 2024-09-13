6ab066aed8f8b55cf349d598acdc8c1675c899d2
TakanoriMoji <t.moji@mates-promo.com>
2024-09-13 14:13:30 +0900

    コミット

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from "react";
+import TodoList from "./TodoList";
+import {v4 as uuidv4 } from "uuid";
 
 function App() {
+const [todos,settodos] = useState([]);
+
+  const todoNameRef = useRef();
+
+  const handleAddTodo =() => {
+    //タスクを追加する。
+    const name = todoNameRef.current.value;
+    if (name === "")return;
+    settodos((prevTodos) => {
+      return [...prevTodos, { id: uuidv4(), name: name, completed: false }];
+    });
+    todoNameRef.current.value = null;
+  };
+
+  const toggleTodo = (id) => {
+    //与えられた引数のidと一致するtodoをtodolistの中から検索し、そのチェック状態を変更する関数
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.completed = !todo.completed;
+    settodos(newTodos);
+  };
+
+  const handleClear = () => {
+    //チェックが入っているtodoを削除する関数
+    const newTodos = todos.filter((todo) => !todo.completed);
+    settodos(newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <input type="text" ref={todoNameRef} />
+      <button onClick={handleAddTodo}>タスクを追加</button>
+      <button onClick={handleClear}>完了したタスクの削除</button>
+      <div>残りのタスク：{todos.filter((todo) => !todo.completed).length}</div>
     </div>
   );
 }
